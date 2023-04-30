0388ebaeeac1de087cc2277dbcb3677a8fedeb25
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2023-04-30 10:04:19 +0300

    Added dynamics to index.php and template-part folder with home-item template.

--- wp-content/themes/homes-softuni/index.php
+++ wp-content/themes/homes-softuni/index.php
@@ -1,43 +1,19 @@
     <?php get_header(); ?>
 
 		<ul class="properties-listing">
-			<li class="property-card">
-				<div class="property-primary">
-					<h2 class="property-title"><a href="#">Two-bedroom apartment</a></h2>
-					<div class="property-meta">
-						<span class="meta-location">Ovcha Kupel, Sofia</span>
-						<span class="meta-total-area">Total area: 91.65 sq.m</span>
-					</div>
-					<div class="property-details">
-						<span class="property-price">€ 100,815</span>
-						<span class="property-date">Posted 14 days ago</span>
-					</div>
-				</div>
-				<div class="property-image">
-					<div class="property-image-box">
-						<img src="wp-content\themes\homes-softuni\assets\images\bedroom.jpg" alt="property image">
-					</div>
-				</div>
-			</li>
 
-			<li class="property-card">
-				<div class="property-primary">
-					<h2 class="property-title"><a href="#">Two-bedroom apartment</a></h2>
-					<div class="property-meta">
-						<span class="meta-location">Ovcha Kupel, Sofia</span>
-						<span class="meta-total-area">Total area: 91.65 sq.m</span>
-					</div>
-					<div class="property-details">
-						<span class="property-price">€ 100,815</span>
-						<span class="property-date">Posted 14 days ago</span>
-					</div>
-				</div>
-				<div class="property-image">
-					<div class="property-image-box">
-						<img src="wp-content\themes\homes-softuni\assets\images\bedroom.jpg" alt="property image">
-					</div>
-				</div>
-			</li>
+		<?php if ( have_posts() ) : ?>
+
+        	<?php while( have_posts() ) : the_post(); ?>
+
+				<?php get_template_part( 'template-parts/home', 'item' ); ?>
+
+			<?php endwhile; ?>
+
+		<?php posts_nav_link(); ?>
+
+		<?php endif; ?>
+			
 		</ul>
 		
 		<?php get_footer(); ?>
---
+++ wp-content/themes/homes-softuni/template-parts/home-item.php
@@ -0,0 +1,18 @@
+<li class="property-card">
+				<div class="property-primary">
+					<h2 class="property-title"><a href="#">Two-bedroom apartment</a></h2>
+					<div class="property-meta">
+						<span class="meta-location">Ovcha Kupel, Sofia</span>
+						<span class="meta-total-area">Total area: 91.65 sq.m</span>
+					</div>
+					<div class="property-details">
+						<span class="property-price">€ 100,815</span>
+						<span class="property-date">Posted 14 days ago</span>
+					</div>
+				</div>
+				<div class="property-image">
+					<div class="property-image-box">
+						<img src="wp-content\themes\homes-softuni\assets\images\bedroom.jpg" alt="property image">
+					</div>
+				</div>
+			</li>
