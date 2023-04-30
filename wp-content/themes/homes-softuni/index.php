f705eee7d300c785a2ef1710f8553f0c10844b57
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2023-04-30 09:48:38 +0300

    Added header and dynamic footer.

--- wp-content/themes/homes-softuni/index.php
+++ wp-content/themes/homes-softuni/index.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Properties</title>
-	<link rel="preconnect" href="https://fonts.gstatic.com">
-
-	<link rel="stylesheet" href="./css/master.css">
-	<link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet">
-</head>
-<body>
-	<div class="site-wrapper">
-		<header class="site-header">
-			<h1 class="site-title"><a href="#">Properties Offers</a></h1>
-		</header>
+    <?php get_header(); ?>
 
 		<ul class="properties-listing">
 			<li class="property-card">
@@ -56,14 +40,4 @@
 			</li>
 		</ul>
 		
-		<footer class="site-footer">
-			<p>&copy; Copyright 2023 | Developer links: 
-				<a href="/edits.html">Edits</a>,
-				<a href="/index.html">Home</a>,
-				<a href="/single.html">Single</a>
-			</p>
-		</footer>
-	</div>
-
-</body>
-</html>
+		<?php get_footer(); ?>
