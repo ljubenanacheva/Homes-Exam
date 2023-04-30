be0171de2467d037ef47a1f5a2d4f506959d0dfa
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2023-04-30 11:01:05 +0300

    Created basic plugin structure.

---
+++ wp-content/plugins/softuni-homes/softuni-homes.php
@@ -0,0 +1,22 @@
+
+<?php
+/*
+ * Plugin Name:       SoftUnit Homes
+ * Plugin URI:        https://softuni.bg
+ * Description:       Basic plugin for homes
+ * Version:           0.1
+ * Requires at least: 5
+ * Requires PHP:      8.0
+ * Author:            Lyubena Nacheva
+ * Author URI:        https://softuni.bg
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       softuni-homes
+ * Domain Path:       /languages
+ */
+
+// Load Custom Post Types
+require 'cpt-homes.php';
+
+// Load our basic functions
+include 'functions.php';
