666f2a6ade16013a05e74444236d2dca155ba318
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2023-04-30 10:09:59 +0300

    Added dynamics to the home posts.

--- wp-content/themes/homes-softuni/template-parts/home-item.php
+++ wp-content/themes/homes-softuni/template-parts/home-item.php
@@ -1,18 +1,30 @@
 <li class="property-card">
 				<div class="property-primary">
-					<h2 class="property-title"><a href="#">Two-bedroom apartment</a></h2>
+					<h2 class="property-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 					<div class="property-meta">
 						<span class="meta-location">Ovcha Kupel, Sofia</span>
 						<span class="meta-total-area">Total area: 91.65 sq.m</span>
 					</div>
 					<div class="property-details">
 						<span class="property-price">€ 100,815</span>
-						<span class="property-date">Posted 14 days ago</span>
+						<span class="property-date">Posted <?php the_date(); ?></span>
 					</div>
 				</div>
 				<div class="property-image">
 					<div class="property-image-box">
-						<img src="wp-content\themes\homes-softuni\assets\images\bedroom.jpg" alt="property image">
+
+                    <?php
+                        if ( has_post_thumbnail() ) {
+
+                            the_post_thumbnail();
+
+                        } else {
+
+                         echo '<img src="<img src="wp-content\themes\homes-softuni\assets\images\bedroom.jpg" alt="property image">';
+                         
+                        }
+                    ?>
+						
 					</div>
 				</div>
 			</li>
