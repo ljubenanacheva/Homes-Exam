5d703b5539d4a2e46c713ee313990ac93127634e
Lyubena Nacheva <32265269+ljubenanacheva@users.noreply.github.com>
2023-04-30 09:58:48 +0300

    Added enqueue function for assets.

---
+++ wp-content/themes/homes-softuni/functions.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ *  This function takes care to enqueue the assets of the project
+ */
+function softuni_assets() {
+    wp_enqueue_style( 'softuni-jobs', get_stylesheet_directory_uri() . '/assets/css/master.css', array(), filemtime(  get_template_directory() . '/assets/css/master.css' ) );
+}
+add_action( 'wp_enqueue_scripts', 'softuni_assets' );
